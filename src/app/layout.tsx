042d0b360f466a77541883cc46d4829038303570
shima8823 <taiyaki1275@gmail.com>
2023-10-01 12:29:50 +0900

    feat: サーバーレンダリングするHeaderServerコンポーネントに変更する

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Header from './components/Header'
+import HeaderServer from './components/HeaderServer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header />
+        <HeaderServer />
         {children}
       </body>
     </html>
