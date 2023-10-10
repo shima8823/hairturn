22495a9bae2c1eba6e7e4e954cc28a8d90204636
shima8823 <taiyaki1275@gmail.com>
2023-10-10 11:17:27 +0900

    feat: Retrieve methodを追加する

--- server/domain/repository/account.go
+++ server/domain/repository/account.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"zubohair/server/domain/object"
 )
 
 type Account interface {
 	Create(ctx context.Context, id string) (sql.Result, error)
+	Retrieve(ctx context.Context, id string) (*object.Account, error)
 }
