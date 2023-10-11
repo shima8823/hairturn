cab2da28584567b0880af9cb229ed23b52d73d9c
shima8823 <taiyaki1275@gmail.com>
2023-10-11 13:26:17 +0900

    feat: cardの登録、閲覧モーダルを追加する

--- src/app/components/HairStyleControls.tsx
+++ src/app/components/HairStyleControls.tsx
@@ -1,31 +1,91 @@
 'use client'
 
-import React from 'react'
+import { useState } from 'react'
 import { Button } from 'react-bootstrap'
+import { cardData } from '../cardUtils'
+import RegisterCardModal from './RegisterCardModal'
+import { Session } from '@supabase/supabase-js'
+import DisplayCard from './DisplayCard'
 
-export default function HairStyleControls(props: { addHair: () => void }) {
+export default function HairStyleControls({
+  cards,
+  session
+}: {
+  cards: cardData[]
+  session: Session | null
+}) {
+  //   DisplayCard
+  const [showRandomCard, setShowRandomCard] = useState(false)
+  const [selectedCard, setSelectedCard] = useState<cardData | null>(null)
+  const selectRandomCard = () => {
+    console.log('selectRandomCard')
+    setSelectedCard(cards[Math.floor(Math.random() * cards.length)])
+    setShowRandomCard(true)
+  }
+  const handleCloseRandomCard = () => {
+    setShowRandomCard(false)
+    setSelectedCard(null)
+  }
+
+  //   RegisterCardModal
+  const [show, setShow] = useState(false)
+
+  const handleShow = () => {
+    setShow(true)
+    console.log('show')
+  }
+
+  const handleClose = () => {
+    setShow(false)
+  }
+  console.log(session === null)
+
   return (
-    // 右寄り
-    <div className="d-flex justify-content-end">
-      {/* 検索バー */}
-      <input
-        type="text"
-        placeholder="Search.."
-        name="search"
-        className="mr-3"
-      />
+    <div>
+      {session && (
+        <RegisterCardModal
+          show={show}
+          handleClose={handleClose}
+          session={session}
+        />
+      )}
+      {selectedCard && (
+        <DisplayCard
+          show={showRandomCard}
+          handleClose={handleCloseRandomCard}
+          card={selectedCard}
+        />
+      )}
 
-      {/* ソート */}
-      <select name="cars" id="cars" className="mr-3">
-        <option value="volvo">New</option>
-        <option value="saab">Old</option>
-      </select>
-
-      {/* New Card modal */}
-
-      <Button variant="primary" onClick={props.addHair}>
-        追加
+      <Button
+        variant="primary"
+        onClick={selectRandomCard}
+        // className={styles['random-button']}
+      >
+        New Hair
       </Button>
+      {/* 右寄り */}
+      <div className="d-flex justify-content-end">
+        {/* 検索バー */}
+        <input
+          type="text"
+          placeholder="Search.."
+          name="search"
+          className="mr-3"
+        />
+        {/* ソート */}
+        <select name="cars" id="cars" className="mr-3">
+          <option value="volvo">New</option>
+          <option value="saab">Old</option>
+        </select>
+        {/* New Card modal */}
+
+        {session && (
+          <Button variant="primary" onClick={handleShow}>
+            追加
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
