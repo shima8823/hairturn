9fec9ff7c22464fd11857d15cdda6028b73b42c5
shima8823 <taiyaki1275@gmail.com>
2023-10-11 12:12:11 +0900

    feat: Delete Methodを追加する

--- server/handler/hairstyles/index.go
+++ server/handler/hairstyles/index.go
@@ -81,3 +81,30 @@ func (h *Handler) Retrieve(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
+	id, err := server.Authenticate(w, r)
+	if err != nil {
+		println("authenticate error")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if id == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+
+	var req object.HairStyle
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.HairStyleRepo.Delete(r.Context(), req.Title)
+	if err != nil {
+		println("delete error")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
