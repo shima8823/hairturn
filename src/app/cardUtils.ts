38f82544a8f8e22081cc39d64ab6058b6b58e15e
shima8823 <taiyaki1275@gmail.com>
2023-10-11 13:23:08 +0900

    refactor: cardData型の変数名を変更する

--- src/app/cardUtils.ts
+++ src/app/cardUtils.ts
@@ -1,33 +1,33 @@
 interface cardData {
-  image: string
+  image_url: string | null
   title: string
   description: string | null
 }
 
 const cardsData: cardData[] = [
   {
-    image: 'hair500.webp',
     title: 'Card 1',
+    image_url: 'hair500.webp',
     description: 'This is the content of Card 1.'
   },
   {
-    image: 'hair1000.webp',
     title: 'Card 2',
+    image_url: 'hair1000.webp',
     description: 'This is the content of Card 2.'
   },
   {
-    image: '',
     title: 'Card 3',
+    image_url: '',
     description: 'This is the content of Card 3.'
   },
   {
-    image: '',
     title: 'Card 4',
+    image_url: '',
     description: 'This is the content of Card 4.'
   },
   {
-    image: '',
     title: 'Card 5',
+    image_url: '',
     description: 'This is the content of Card 5.'
   }
 ]
