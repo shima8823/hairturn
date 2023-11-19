2702edcfa7742dbda8a7c9a445eeeae79f39430b
shima8823 <taiyaki1275@gmail.com>
2023-11-19 19:39:00 +0900

    feat: 履歴に追加ボタンを追加する

--- src/app/components/DisplayCard.tsx
+++ src/app/components/DisplayCard.tsx
@@ -1,35 +1,60 @@
 'use client'
-import { Modal } from 'react-bootstrap'
+import { Modal, Button } from 'react-bootstrap'
 import Image from 'next/image'
+import { Session } from '@supabase/auth-helpers-nextjs'
 
-import { cardData } from '../../lib/card-data'
+import { Database } from '@/lib/database.types'
+type Hairstyle = Database['public']['Tables']['hairstyles']['Row']
 
 export default function DisplayCard(props: {
   show: boolean
   handleClose: () => void
-  card: cardData
+  hairstyle: Hairstyle
+  session: Session | null
 }) {
   const handleClose = () => {
     props.handleClose()
   }
 
+  const handleAddHistory = async () => {
+    if (!props.session) return
+    // cardのidとuseridが欲しい
+    const res = await fetch('/api/hairstyle-history', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(props.hairstyle)
+    })
+    if (!res.ok) {
+      alert('履歴の追加に失敗しました。')
+      return
+    }
+    alert('履歴に追加しました。')
+    handleClose()
+  }
+
   return (
     <Modal show={props.show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>{props.card?.title}</Modal.Title>
+        <Modal.Title>{props.hairstyle.title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {props.card && props.card.image_url && (
+        {props.hairstyle.image_url && (
           <Image
-            src={props.card?.image_url}
-            alt={props.card?.title}
+            src={props.hairstyle.image_url}
+            alt={props.hairstyle.title}
             width={450}
             height={450}
             layout="responsive"
           />
         )}
-        <p>{props.card?.description}</p>
+        <p>{props.hairstyle.description}</p>
       </Modal.Body>
+
+      <Modal.Footer>
+        <Button onClick={handleAddHistory}>履歴に追加</Button>
+      </Modal.Footer>
     </Modal>
   )
 }
