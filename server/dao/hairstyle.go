a65b757d72f28ca241eb859145f0da0f3e82134f
shima8823 <taiyaki1275@gmail.com>
2023-11-22 18:37:43 +0900

    feat: 履歴と髪型のdb関係を無駄がないようにする

--- server/dao/hairstyle.go
+++ server/dao/hairstyle.go
@@ -46,7 +46,7 @@ func (r *hairstyle) Create(ctx context.Context, hairstyle *object.HairStyle) (sq
 }
 
 func (r *hairstyle) Retrieve(ctx context.Context, user_id string) ([]*object.HairStyle, error) {
-	const retrieve = "SELECT id, user_id, image_url, title, description, created_at, updated_at FROM hairstyles WHERE user_id = $1"
+	const retrieve = "SELECT id, user_id, image_url, title, description, created_at, updated_at FROM hairstyles WHERE user_id = $1 AND is_deleted = false"
 
 	var rows *sql.Rows
 	var err error
@@ -75,21 +75,53 @@ func (r *hairstyle) Retrieve(ctx context.Context, user_id string) ([]*object.Hai
 }
 
 func (r *hairstyle) Delete(ctx context.Context, title string) error {
+	const retrieveHairstyles = "SELECT * FROM hairstyles WHERE title = $1"
+	const retrieveHairstyleHistory = "SELECT hairstyle_id FROM hairstyle_history WHERE hairstyle_id = $1"
 	const delete = "DELETE FROM hairstyles WHERE title = $1"
-	res, err := r.db.ExecContext(ctx, delete, title)
+	const update = "UPDATE hairstyles SET is_deleted = true WHERE title = $1"
+
+	tx, err := r.db.BeginTxx(ctx, nil)
 	if err != nil {
 		return err
 	}
-	
-	affect, err := res.RowsAffected()
-	if err != nil {
-		return  err
+
+	// hairstylesからidを取得
+	hairstyle := object.HairStyle{}
+	if err := tx.QueryRowxContext(ctx, retrieveHairstyles, title).StructScan(&hairstyle); err != nil {
+		tx.Rollback()
+		return err
 	}
 
-	if affect == 0 {
-		return sql.ErrNoRows
+	// hairstyle_historyからidを取得
+	var historyId int
+	if err := tx.QueryRowContext(ctx, retrieveHairstyleHistory, hairstyle.Id).Scan(&historyId); err != nil {
+		if err == sql.ErrNoRows {
+			// 物理削除
+			if _, err := tx.ExecContext(ctx, delete, title); err != nil {
+				tx.Rollback()
+				return err
+			}
+			if err := deleteHairstyleImage(ctx, hairstyle); err != nil {
+				tx.Rollback()
+				return err
+			}
+		} else {
+			tx.Rollback()
+			return err
+		}
+	} else {
+		// 論理削除
+		if _, err := tx.ExecContext(ctx, update, title); err != nil {
+			tx.Rollback()
+			return err
+		}
 	}
-	
+
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return err
+	}
+
 	return nil
 }
 
--- server/dao/hairstyle_history.go
+++ server/dao/hairstyle_history.go
@@ -20,12 +20,82 @@ func NewHairStyleHistory(db *sqlx.DB) repository.HairStyleHistory {
 }
 
 func (r *hairstyleHistory) Create(ctx context.Context, hairstyleHistory *object.HairStyleHistory) (sql.Result, error) {
-	const create = "INSERT INTO hairstyle_history (user_id, hairstyle_id) VALUES ($1, $2)"
-
-	res, err := r.db.ExecContext(ctx, create, hairstyleHistory.UserId, hairstyleHistory.HairstyleId)
+	tx, err := r.db.BeginTxx(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
+
+	const create = "INSERT INTO hairstyle_history (user_id, hairstyle_id) VALUES ($1, $2)"
+	res, err := tx.ExecContext(ctx, create, hairstyleHistory.UserId, hairstyleHistory.HairstyleId)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	const countQuery = "SELECT COUNT(*) FROM hairstyle_history WHERE user_id = $1"
+	var count int
+	err = tx.QueryRowContext(ctx, countQuery, hairstyleHistory.UserId).Scan(&count)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	// エントリ数が12個以上の場合、最も古いエントリを削除
+	if count > 12 {
+		const oldestQuery = "SELECT * FROM hairstyle_history WHERE user_id = $1 ORDER BY created_at ASC LIMIT 1"
+		oldestHairstyleHistory := object.HairStyleHistory{}
+		err = tx.QueryRowxContext(ctx, oldestQuery, hairstyleHistory.UserId).StructScan(&oldestHairstyleHistory)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+
+		// hairstylesテーブルに存在するか確認
+		const existsQuery = "SELECT * FROM hairstyles WHERE id = $1"
+		hairstyle := object.HairStyle{}
+		err = tx.QueryRowxContext(ctx, existsQuery, oldestHairstyleHistory.HairstyleId).StructScan(&hairstyle)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+		// hairstyle_historyの中からhairstyle.Idを持つものの数を数える
+		const countQuery = "SELECT COUNT(*) FROM hairstyle_history WHERE hairstyle_id = $1"
+		var count int
+		err = tx.QueryRowContext(ctx, countQuery, oldestHairstyleHistory.HairstyleId).Scan(&count)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+
+		if *hairstyle.IsDeleted && count == 1 {
+			// hairstyleの物理削除
+			const deleteQuery = "DELETE FROM hairstyles WHERE id = $1"
+			_, err = tx.ExecContext(ctx, deleteQuery, oldestHairstyleHistory.HairstyleId)
+			if err != nil {
+				tx.Rollback()
+				return nil, err
+			}
+			if err := deleteHairstyleImage(ctx, hairstyle); err != nil {
+				tx.Rollback()
+				return nil, err
+			}
+		}
+
+		// hairstyle_historyの物理削除
+		const deleteQuery = "DELETE FROM hairstyle_history WHERE id = $1"
+		_, err = tx.ExecContext(ctx, deleteQuery, oldestHairstyleHistory.Id)
+		if err != nil {
+			tx.Rollback()
+			return nil, err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
 	return res, nil
 }
 
