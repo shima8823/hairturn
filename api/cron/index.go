deb902ba2364a7eb4f2926a2eea3d9d4ebfc5a47
shima8823 <taiyaki1275@gmail.com>
2023-11-23 18:39:16 +0900

    initial commit

---
+++ api/cron/index.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"net/http"
+	"hairturn/server/dao"
+	"hairturn/server/handler/account"
+
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	db, err := dao.GetDBConnection(db)
+	if err != nil {
+		panic(err)
+	}
+	dao := dao.New(db)
+	accountHandler := account.NewHandler(dao.Account())
+
+	switch r.Method {
+	case http.MethodGet:
+		accountHandler.SendReminderMail(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
