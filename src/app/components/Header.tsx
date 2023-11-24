efef25e66babeccd29da3450778fec79e80fbe05
shima8823 <taiyaki1275@gmail.com>
2023-11-24 15:05:49 +0900

    feat: account pageを追加する

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -32,6 +32,9 @@ export default function Header({ session }: { session: Session | null }) {
               <Nav className="me-auto"></Nav>
               {session ? (
                 <NavDropdown title="マイページ" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/account">
+                    アカウント
+                  </NavDropdown.Item>
                   <NavDropdown.Item href="/hairstyle-history">
                     履歴
                   </NavDropdown.Item>
