fbd22d577ab1ca14a1f20e71b975bac119300384
shima8823 <taiyaki1275@gmail.com>
2023-11-27 17:53:35 +0900

    feat: faviconを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HeaderServer from './components/HeaderServer'
+import Favicon from '/public/favicon.ico'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'HairTurn',
-  description: 'HairTurnは、髪型を管理するためのアプリです。'
+  description: 'HairTurnは、髪型を管理するためのアプリです。',
+  icons: [{ rel: 'icon', url: Favicon.src }]
 }
 
 export default function RootLayout({
